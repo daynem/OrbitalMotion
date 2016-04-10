73f677f018e04bf56f86699baa8698185d19d3e5
daynem <orbitsim@gmail.com>
2016-04-10 16:57:04 -0600

    Initial implementation

---
+++ src/com/medlyn/oribitalmotion/CelestialBody.java
@@ -0,0 +1,16 @@
+package com.medlyn.oribitalmotion;
+/**
+ * Created by Dayne on 4/9/2016.
+ */
+public class CelestialBody {
+    // Body mass
+    double mass = 0.0;
+
+    // Body Position
+    double xPosition = 0.0;
+    double yPosition = 0.0;
+
+    // Body Velocity
+    double xVelocity = 0.0;
+    double yVelocity = 0.0;
+}
---
+++ src/com/medlyn/oribitalmotion/MotionPanel.java
@@ -0,0 +1,59 @@
+package com.medlyn.oribitalmotion;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by Dayne on 4/10/2016.
+ */
+class MotionPanel extends JPanel {
+
+    private int squareX = 50;
+    private int squareY = 50;
+    private int squareW = 20;
+    private int squareH = 20;
+
+    public MotionPanel() {
+
+        setBorder(BorderFactory.createLineBorder(Color.black));
+
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                moveSquare(e.getX(),e.getY());
+            }
+        });
+
+    }
+
+    private void moveSquare(int x, int y) {
+        int OFFSET = 1;
+        if ((squareX!=x) || (squareY!=y)) {
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+            squareX=x;
+            squareY=y;
+            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+        }
+    }
+
+
+    public Dimension getPreferredSize() {
+        return new Dimension(920, 600);
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawString("This is my custom Panel!",10,20);
+        g.setColor(Color.RED);
+        g.fillRect(squareX,squareY,squareW,squareH);
+        g.setColor(Color.BLACK);
+        g.drawRect(squareX,squareY,squareW,squareH);
+    }
+}
